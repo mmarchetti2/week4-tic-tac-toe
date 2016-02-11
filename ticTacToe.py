4f1124153ec47fa3aba9f72d4844e63c7bd3688a
Matthew Marchetti <mmarchetti2@mail.valenciacollege.edu>
2016-02-10 23:41:03 -0500

    Week 4's Work
    I added the code necessary to make the program work. It now works
    perfectly fine from the runner file. :)

--- ticTacToe.py
+++ ticTacToe.py
@@ -6,10 +6,62 @@ def printBoard(board):
     #                                                                       #
     # Hint: you can follow the same process that was done in the textbook.  #
     #########################################################################
+    print(board['top-L'] + '|' + board['top-M'] + '|' + board['top-R'])
+    print('-+-+-')
+    print(board['mid-L'] + '|' + board['mid-M'] + '|' + board['mid-R'])
+    print('-+-+-')
+    print(board['low-L'] + '|' + board['low-M'] + '|' + board['low-R'])
 
 def checkWinner(board, player):    
     print('Checking if ' + player + ' is a winner...')
-    
+    if board['top-L'] == player:
+        if board['mid-L'] == player:
+            if board['low-L'] == player:
+                return True
+        else:
+            return False
+    if board['top-M'] == player:
+        if board['mid-M'] == player:
+            if board['low-M'] == player:
+                return True
+        else:
+            return False
+    if board['top-R'] == player:
+        if board['mid-R'] == player:
+            if board['low-R'] == player:
+                return True
+        else:
+            return False
+    if board['top-L'] == player:
+        if board['top-M'] == player:
+            if board['top-R'] == player:
+                return True
+        else:
+            return False
+    if board['mid-L'] == player:
+        if board['mid-M'] == player:
+            if board['mid-R'] == player:
+                return True
+        else:
+            return False
+    if board['low-L'] == player:
+        if board['low-M'] == player:
+            if board['low-R'] == player:
+                return True
+        else:
+            return False
+    if board['top-L'] == player:
+        if board['mid-M'] == player:
+            if board['low-R'] == player:
+                return True
+        else:
+            return False
+    if board['top-R'] == player:
+        if board['mid-M'] == player:
+            if board['low-L'] == player:
+                return True
+        else:
+            return False
     # TO DO #################################################################
     # Write code in this function that checks the tic-tac-toe board          #
     # to determine if the player stored in variable 'player' currently      #
@@ -18,31 +70,30 @@ def checkWinner(board, player):
     # variable 'player' has won. The function should return False           #
     # if the player in the variable 'player' has not won.                   #
     #########################################################################
-    
-    
+       
 def startGame(startingPlayer, board):
     # TO DO #################################################################
     # Add comments to each line in this function to describe what           #
     # is happening. You do not need to modify any of the Python code        #
     #########################################################################
 
-    turn = startingPlayer
-    for i in range(9):
-        printBoard(board)
-        print('Turn for ' + turn + '. Move on which space?')
-        move = input()
-        board[move] = turn
-        if( checkWinner(board, 'X') ):
-            print('X wins!')
-            break
-        elif ( checkWinner(board, 'O') ):
-            print('O wins!')
-            break
+    turn = startingPlayer #Lets the starting player (X or O) start.
+    for i in range(9): #Allows for 9 turns, as there are 9 spots on the board.
+        printBoard(board) #Prints the board.
+        print('Turn for ' + turn + '. Move on which space?') #Tells which player is to take his/her turn next.
+        move = input() #Takes the input of the player's move.
+        board[move] = turn 
+        if( checkWinner(board, 'X') ): #Checks if X wins,
+            print('X wins!') #And prints if it does
+            break #Breaks the loop
+        elif ( checkWinner(board, 'O') ): #Checks if O wins,
+            print('O wins!') #And prints if it does
+            break #Breaks the loop
     
-        if turn == 'X':
+        if turn == 'X': 
             turn = 'O'
         else:
-            turn = 'X'
+            turn = 'X' #The above 3 lines switches between X and O's turn
         
-    printBoard(board)
+    printBoard(board) #Prints the board
     
